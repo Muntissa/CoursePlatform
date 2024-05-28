b886525a7e4fa55c49f606c2d6683a0d67a805eb
Muntissa <h3r1zma@gmail.com>
2024-05-28 03:08:59 +0300

    Constructor

---
+++ CoursePlatform/Pages/CourseConstructor.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using CoursePlatform.Common;
+using CoursePlatform.Common.Additional;
+using CoursePlatform.Common.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using static System.Net.WebRequestMethods;
+
+namespace CoursePlatform.Pages
+{
+    public class CourseConstructor : PageModel
+    {
+        private readonly CoursePlatformContext _context;
+        private readonly UserManager<User> _userManager;
+
+        public CourseConstructor(CoursePlatformContext context, UserManager<User> userManager)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public Course Course { get; set; }
+        public Lecture CurrentLecture { get; set; }
+
+        public void OnGet(int? courseid, int? lectureid)
+        {
+            if (courseid is null)
+                return;
+
+            Course = _context.Set<Course>()
+                .Include(c => c.Lectures)
+                .FirstOrDefault(c => c.Id == courseid);
+
+            CurrentLecture = Course.Lectures.Where(l => l.Id == lectureid).FirstOrDefault();
+        }
+
+        public async Task<IActionResult> OnPost(int courseid)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            _context.Set<Course>()
+                .Include(c => c.CourseEnrollments)
+                .FirstOrDefault(c => c.Id == courseid)
+                .CourseEnrollments.Add(new CourseEnrollment() { EnrollmentDate = DateTime.Now, Student = user });
+
+            _context.SaveChanges();
+
+            return RedirectToPage(new { courseid });
+        }
+    }
+}
